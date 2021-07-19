91e46c8025ccba4c2d818fe719250b9b4468a995
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2021-07-19 21:22:53 +0100

    commit

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''type notations'''
+from typing import TypeVar, Mapping, Any, Union
+
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[TypeVar('T'), None])\
+                     -> Union[Any, TypeVar('T')]:
+    '''function safety_get_value'''
+    if key in dct:
+        return dct[key]
+    else:
+        return default
