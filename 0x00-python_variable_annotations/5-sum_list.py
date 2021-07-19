7496c19032824f349e130283c7ea820156ff4e4d
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2021-07-19 20:49:36 +0100

    commit

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+
+#!/usr/bin/env python3
+'''a type-annotated function sum_list'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''function sum_list'''
+    return sum(input_list)
