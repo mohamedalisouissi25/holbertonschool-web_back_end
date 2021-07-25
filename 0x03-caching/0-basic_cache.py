277cb1ef369b2bd363bbcbc8efe769c90cd04921
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2021-07-25 15:05:21 +0100

    commit

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -1,35 +1,20 @@
 #!/usr/bin/python3
-"""caching"""
-
-BaseCaching = __import__('base_caching').BaseCaching
+"""Caching"""
 
 
-class BasicCache(BaseCaching):
-    """A basic caching system class"""
+BasicCaching = __import__('base_caching').BaseCaching
 
-    def __init__(self):
-        super().__init__()
 
-    def put(self, key, value):
-        """[insert data to the cache]
-        Args:
-            key ([string]): [description]
-            value ([type]): [description]
-        """
-
-        if key is None or value is None:
-            return
-        self.cache_data[key] = value
+class BasicCache(BasicCaching):
+    """cache class"""
+    def put(self, key, item):
+        """function put"""
+        if key and item:
+            self.cache_data[key] = item
 
     def get(self, key):
-        """[summary]
-        Args:
-            key ([string]): [description]
-        Returns:
-            [None or value]: [description]
-        """
-
-        if key is None or key not in self.cache_data.keys():
+        """function get"""
+        if key not in self.cache_data.keys():
             return None
-
-        return self.cache_data[key]
+        else:
+            return self.cache_data[key]
